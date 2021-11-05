a7e576c3b2d08eed65cc2ac70ae769a7e082090e
hka <hoangkianh31592@gmail.com>
2021-11-05 14:20:24 +0700

    build: add react-bootstrap

--- src/components/Header/Styled.tsx
+++ src/components/Header/Styled.tsx
@@ -1,7 +1,18 @@
 import styled, { css } from 'styled-components/macro'
+import { Container } from 'react-bootstrap'
 
 export const HeaderWrapper = styled.header`
-  min-height: 80px;
   background: ${props => props.theme.color.headerBg};
   box-shadow: rgba(0, 0, 0, .4) 0px 3px 5px;
+`
+export const HeaderContainer = styled(Container)`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0 auto;
+  min-height: 80px;
+  max-width: 1280px;
+`
+export const Logo = styled.img`
+  height: auto;
 `
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,16 @@
-import { HeaderWrapper } from './Styled'
+import { HeaderWrapper, HeaderContainer, Logo } from './Styled'
+import { Button } from 'react-bootstrap'
+import LogoPNG from '../../assets/images/logo.png'
 
 function Header(): JSX.Element {
   return (
     <HeaderWrapper>
+      <HeaderContainer>
+        <Logo src={LogoPNG} alt="DotOracle" />
+        <div>
+          <Button>Connect Wallet</Button>
+        </div>
+      </HeaderContainer>
     </HeaderWrapper>
   )
 }
