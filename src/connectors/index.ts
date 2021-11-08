4afb37f3203d0eb363f62b202f5f887739cbce18
hka <hoangkianh31592@gmail.com>
2021-11-09 01:46:04 +0700

    fix: connectors

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -22,7 +22,6 @@ export const walletconnect = new WalletConnectConnector({
   rpc: { [NETWORK_CHAIN_ID]: NETWORK_URL },
   bridge: 'https://bridge.walletconnect.org',
   qrcode: true,
-  pollingInterval: 15000,
 })
 
 export const network = new NetworkConnector({
