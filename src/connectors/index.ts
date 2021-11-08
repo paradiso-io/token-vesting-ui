28ef7b326cf49ff6855b54ba05dfd1c160abe450
hka <hoangkianh31592@gmail.com>
2021-11-08 10:23:27 +0700

    build: add network connectors

---
+++ src/connectors/index.ts
@@ -0,0 +1,35 @@
+import { InjectedConnector } from '@web3-react/injected-connector'
+import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
+import { NetworkConnector } from './NetworkConnector'
+
+export enum ConnectorNames {
+  Injected = 'injected',
+  WalletConnect = 'walletconnect',
+}
+
+const NETWORK_URL = process.env.REACT_APP_RPC_URL
+export const NETWORK_CHAIN_ID: number = parseInt(process.env.REACT_APP_CHAIN_ID ?? '42')
+
+if (typeof NETWORK_URL === 'undefined') {
+  throw new Error(`REACT_APP_NETWORK_URL must be a defined environment variable`)
+}
+
+export const injected = new InjectedConnector({
+  supportedChainIds: [56, 97],
+})
+
+export const walletconnect = new WalletConnectConnector({
+  rpc: { [NETWORK_CHAIN_ID]: NETWORK_URL },
+  bridge: 'https://bridge.walletconnect.org',
+  qrcode: true,
+  pollingInterval: 15000,
+})
+
+export const network = new NetworkConnector({
+  urls: { [NETWORK_CHAIN_ID]: NETWORK_URL },
+})
+
+export const connectorsByName: { [connectorName in ConnectorNames]: any } = {
+  [ConnectorNames.Injected]: injected,
+  [ConnectorNames.WalletConnect]: walletconnect,
+}
